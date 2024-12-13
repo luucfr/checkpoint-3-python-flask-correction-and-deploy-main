d741755134b8309faed5084dd341f648e9be1ad1
luucfr <contact@luuc.fr>
2024-12-13 09:03:52 +0000

    Ajout de la prise en charge des requêtes GET et POST dans la route principale, désactivation du mode debug et remplacement du fichier index.html par une nouvelle version.

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 servers = []
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
@@ -16,4 +15,4 @@ def index():
     return render_template('index.html', servers=servers)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
